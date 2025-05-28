f2eec486de88de4690ff9c03d0765f0deb6808af
CP_DevOps_Team <devops@controlpass.hu>
2025-05-28 10:19:26 +0200

    Update Projects component to use local images

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
